b116e0ecc077163fcd1aa1342c28cbd906b25179
PhuongTa <phuong.ta@merkle.com>
2024-03-15 23:43:58 +0700

    connect database, render data from database, tranfer data detail page via param

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Implement a simple project with nodejs
 npm install --save-exact body-parser@1.19.0 nodemon@2.0.12 @babel/core@7.15.5 @babel/node@7.15.4 @babel/preset-env@7.15.6
 
 npm install --save-exact dotenv@10.0.0
+
+npm install --save -exact mysql2@2.3.0
--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 import express from 'express';
 import configViewEngine from './configs/viewEngine';
-require('dotenv').config();
+import initWebRouter from './router/web';
+// import connection from './configs/connectDB';
 
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT;
 
 //config view
 configViewEngine(app);
+//create router
+initWebRouter(app);
 
-
-app.get('/', (req, res) => {
-    // res.send('Hello World! This is express on node js 8080')
-    res.render('test/index.ejs');
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
