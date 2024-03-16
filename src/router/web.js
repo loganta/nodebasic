f34cc2d310b7022cef02f321fd6b23832f449dbd
PhuongTa <phuong.ta@merkle.com>
2024-03-16 20:58:46 +0700

    upload single file to server

--- src/router/web.js
+++ src/router/web.js
@@ -1,7 +1,36 @@
 import express from "express";
 import homeController from '../controller/homeController';
+import multer from "multer";
+import path from 'path';
+
+var appRoot = require('app-root-path');
+
 //use Router service of express framework js
 let router = express.Router();
+
+//upload file variable
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, appRoot + "/src/public/image/");
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const imageFilter = function (req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+//end upload file variable
 
 const initWebRoute = (app) => {
     //response the action show homepage
@@ -21,6 +50,15 @@ const initWebRoute = (app) => {
 
     //method post data to update data after edit action
     router.post('/update-user', homeController.postUpdateUser);
+
+    /*----------------------------------------------------*/
+
+    //respon the page content upload file
+    router.get('/upload', homeController.getUploadFilePage);
+    //handle upload file function
+    router.post('/upload-profile-pic', upload.single('profile_pic'), homeController.handleUploadFile)
+
+    /*----------------------------------------------------*/
 
     //response the action show about page
     router.get('/about', (req, res) => {
